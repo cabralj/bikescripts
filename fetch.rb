a061f2590631fc0799875413a1f324288bd4aaa7
cabral <cabralj@gmail.com>
2014-11-11 21:47:43 -0500

    fetching detail page

--- fetch.rb
+++ fetch.rb
@@ -1,33 +1,42 @@
+require "open-uri"
 
-require 'open-uri'
-
-require 'net/http'
-require 'uri'
-
+base = "http://bikepedia.com/"
+uagent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
+ref = "http://www.bikepedia.com/"
 
 part = ARGV[0]
-url = "http://bikepedia.com/Search.aspx?Q=" + part
+url = base + "/Search.aspx?Q=" + part
 
-def open(u)
-	puts "Fetching\n" + u
-	Net::HTTP.get(URI.parse(u))
-end
+#url = "http://bikepedia.com90E0-7161"
 
-def saveImg()
-	File.open('pie.png', 'wb') do |fo|
-	  fo.write open("http://chart.googleapis.com/chart?").read 
-	end
-end
+puts "==================================="
+source = open(url,
+	"User-Agent" => uagent,
+	"Referrer" => ref	
+).read
 
+puts "Fetch \t#{url}"
 
-puts logo
+if source.include? '<span id="ctl00_MainContent_searchCountLabel">1</span></b>'
+	puts "Found\t1"	
+	links = source.scan(/<a.+?href="(.+?)".+?/)
+	puts "==================================="
+	
+	#puts links.inspect
 
+	dLink = "http://bikepedia.com" + links[42][0]
+	puts "Fetch \t#{dLink}"
 
-page = open(url)
-image = saveImg()
-
-if page.include? '<span id="ctl00_MainContent_searchCountLabel">1</span></b>'
-	puts "1 Match"
+	detail = open(dLink,
+		"User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+		"Referrer" => "http://www.bikepedia.com/"	
+	).read
+	
 else
-	puts "0 or more"
+	puts "Not Found"
 end
+
+
+
+
+
